58af55f3451162465cbbe6b732024362e72c4076
chantaf <badr.chantaf@gmail.com>
2022-02-16 12:32:12 +0100

    gestion-rdv-covid19

--- covid19/src/app.module.ts
+++ covid19/src/app.module.ts
@@ -4,10 +4,25 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { AdminsModule } from './admin/admin.module';
+import { VaccinsModule } from './vaccin/vaccin.module';
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb://localhost/covid19'), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
+
+// @Module({
+//   imports: [MongooseModule.forRoot('mongodb://localhost/covid19'),AdminsModule ],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
+
+// @Module({
+//   imports: [MongooseModule.forRoot('mongodb://localhost/covid19'),VaccinsModule ],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
+
 export class AppModule {}
